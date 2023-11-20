8f44813c333a5dab0d683ee1ef88149ac910b5a3
kdc119 <skt32@naver.com>
2023-11-20 11:52:35 +0900

    1st

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["gateway.ipfscdn.io"],
+    domains: [
+      'gateway.ipfscdn.io',
+      'bafybeifmmo646ymfr2dstvl6vehwhmqucgqbszj6x4fkmqnqjy4ccfcfn4.ipfs.cf-ipfs.com',
+    ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
